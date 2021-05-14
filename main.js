97eea7746cbb8b6aef02379969fd8aa163943c6b
akm1504 <anandu.poly@gmail.com>
2021-05-14 23:32:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-});
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-});
 const { Telegraf, Markup } = require('telegraf');
 const axios = require('axios');
 
