912da5deaa521362447ec23722488159223c9cdc
akm1504 <anandu.poly@gmail.com>
2021-05-14 16:42:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    BOT_TOKEN: '1352267645:abcdefghifklmnopqrstuvwxyz',
+    BOT_TOKEN: '1716733853:AAHJod03Kig7cZoJD1o_kpf9B9BZFBtrOFA',
     API_URL: 'https://7ajk43.deta.dev/location/'
 }
