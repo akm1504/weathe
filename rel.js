bf2910ad40daf3f827f5bcb6b4c5b8d88e7eb825
akm1504 <anandu.poly@gmail.com>
2021-05-15 13:29:20 +0530

    Update rel.js

--- rel.js
+++ rel.js
@@ -1,12 +1,15 @@
 var a = function a(){
+  var port = 8080;
 var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(8080);
-  
+});
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
 console.log("done"); 
 
 }
