fa3ef1b2a99219dc8993732231379562d6051a1d
akm1504 <anandu.poly@gmail.com>
2021-05-15 13:20:57 +0530

    Update rel.js

--- rel.js
+++ rel.js
@@ -6,7 +6,7 @@ http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 }).listen(8080);
-});
+  
 console.log("done"); 
 
 }
