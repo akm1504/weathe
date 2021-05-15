8a0ecf8ab1cbc3a3603e288736d1d66fb867b1a1
akm1504 <anandu.poly@gmail.com>
2021-05-15 13:00:24 +0530

    Update rel.js

--- rel.js
+++ rel.js
@@ -1,6 +1,11 @@
+var a = function a(){
 var http = require('http');
 
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 }).listen(8080);
+
+};
+
+module.exports.a = a;
